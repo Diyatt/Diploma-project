52cbb0d02f85a732ceaae540f5926dc5414d80b3
Alikhan <dosimovic04@gmail.com>
2025-03-08 16:35:01 +0500

    models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,3 +1,57 @@
 from django.db import models
 
-# Create your models here.
+class Region(models.Model):
+    name = models.CharField(max_length=255)
+
+class District(models.Model):
+    name = models.CharField(max_length=255)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+class User(models.Model):
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
+    district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True)
+    full_name = models.CharField(max_length=255)
+    local_address = models.TextField()
+    phone_number = models.CharField(max_length=20)
+    whatsapp_number = models.CharField(max_length=20, blank=True, null=True)
+    password = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    user_type = models.CharField(max_length=50, choices=[('owner', 'Owner'), ('renter', 'Renter')])
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255)
+
+class Product(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    region = models.ForeignKey(Region, on_delete=models.SET_NULL, null=True)
+    district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255)
+    image_url = models.URLField()
+    price_per_day = models.DecimalField(max_digits=10, decimal_places=2)
+    piece = models.IntegerField()
+    description = models.TextField()
+
+class Review(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    review_date = models.DateTimeField(auto_now_add=True)
+
+class Wishlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+class Chat(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chats_initiated')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chats_received')
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    image_url = models.URLField(blank=True, null=True)
+    sent_at = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
