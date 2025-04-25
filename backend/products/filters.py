bd9e6e0593870b5dc693cfb069d43eede8ecfdca
Alikhan <dosimovic04@gmail.com>
2025-04-25 12:12:23 +0500

    fix bag category filter
    ']\];da
    wl,lwq
    hellp

--- backend/products/filters.py
+++ backend/products/filters.py
@@ -1,11 +1,13 @@
 import django_filters
 from .models import Product
+from django_filters import rest_framework as filters
 
 class ProductFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name="price", lookup_expr="gte")  # Цена >=
     max_price = django_filters.NumberFilter(field_name="price", lookup_expr="lte")  # Цена <=
     district = django_filters.BaseInFilter(field_name="district", lookup_expr="in")
 
+    category = filters.NumberFilter(field_name="category")
 
     class Meta:
         model = Product
