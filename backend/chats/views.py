91103dd9403b43ecd8aa48ebd2ac7767c32509d2
Alikhan <dosimovic04@gmail.com>
2025-05-08 07:09:51 +0500

    fix chat

--- backend/chats/views.py
+++ backend/chats/views.py
@@ -9,7 +9,14 @@ class MessageListCreateView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         chat_id = self.kwargs.get('chat_id')
-        return Message.objects.filter(chat_id=chat_id)
+        try:
+            chat = Chat.objects.get(pk=chat_id)
+            # Check if the current user is part of this chat
+            if self.request.user != chat.user1 and self.request.user != chat.user2:
+                raise ValidationError("You are not a participant in this chat.")
+            return Message.objects.filter(chat_id=chat_id)
+        except Chat.DoesNotExist:
+            raise ValidationError("Chat does not exist.")
 
     def perform_create(self, serializer):
         chat_id = self.kwargs['chat_id']
