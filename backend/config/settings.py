9556723662ab919c4f81f17520857fbfcbda9f6b
Alikhan <dosimovic04@gmail.com>
2025-05-12 10:12:21 +0500

    fix bag

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -220,11 +220,13 @@ BACKEND_URL = os.getenv("BACKEND_URL", "http://127.0.0.1:8000")
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_SSL_CERTVERIFY = False
 
 CHANNEL_LAYERS = {
     'default': {
